19f9df3c899e1373dc8cd5050c601a22397d8074
Kamil <kdnuriev@gmail.com>
2024-02-08 19:00:28 +0400

    ya banan

--- templates/support/js/analytics.js
+++ templates/support/js/analytics.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // ajax request function
     xhr = new XMLHttpRequest();
-    xhr.open('GET', 'moneybuddy-production.up.railway.app/getuserdata', true);
+    xhr.open('GET', 'https://moneybuddy-production.up.railway.app/getuserdata', true);
     xhr.onload = function () {
         if (this.status == 200) {
             var u = JSON.parse(this.responseText);
--- templates/support/js/goals.js
+++ templates/support/js/goals.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // ajax request func
     function Get() {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'moneybuddy-production.up.railway.app/getuserdata', true);
+        xhr.open('GET', 'https://moneybuddy-production.up.railway.app/getuserdata', true);
         xhr.onload = function () {
             if (this.status == 200) {
                 var u = JSON.parse(this.responseText);
@@ -126,7 +126,7 @@ document.addEventListener('DOMContentLoaded', function () {
         location.reload();
 
         var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'moneybuddy-production.up.railway.app/postpiggybank', true);
+        xhr.open('POST', 'https://moneybuddy-production.up.railway.app/postpiggybank', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify(PiggyBank));
         xhr.onload = function() {
